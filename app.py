0d9e2a9ebd6d9d505961048f728e648f0c88a636
James Burch <burchjames90@gmail.com>
2020-08-08 17:03:04 -0600

    implented API key into all requests

--- app.py
+++ app.py
@@ -32,68 +32,104 @@ def id():
     # Check if an ID was provided as part of the URL.
     # If ID is provided, assign it to a variable.
     # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
     else:
-        return "Error: No id field provided. Please specify an id."
-    return jsonify(sitesByOwnerID(id))
+        if 'id' in request.args:
+            id = int(request.args['id'])
+        else:
+            return "Error: No id field provided. Please specify an id."
+        return jsonify(sitesByOwnerID(id))
 
 
 @app.route('/login=<UserName>', methods=['GET'])
 def login(UserName):
-    SitesArray = []
-    OwnerID = ownerIDByUserName(UserName)
-    OwnersSiteIDs = sitesByOwnerID(OwnerID)
-    LoginDict = {"OwnerID" : ownerIDByUserName(UserName)}
-    for MPM in OwnersSiteIDs:
-        #MPMArrayofDicts = []
-        #MPMArrayofDicts = (CordinatorStatusbySiteID(MPM)) + (SiteInfobySiteID(MPM))
-        #MPMArrayofDicts.append({"Swipers": swipersBySiteID(MPM)})
-        #MPMArrayofDicts.append(CordinatorStatusbySiteID(MPM))
-        #MPMArrayofDicts.append(SiteInfobySiteID(MPM))
-        SitesArray.append({**(CordinatorStatusbySiteID(MPM)),
-        **(SiteInfobySiteID(MPM)), **{"Swipers": swipersBySiteID(MPM)},
-        **{"siteID" : MPM}})
-    LoginDict["SiteArray"] = SitesArray
-    return jsonify(LoginDict)
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        SitesArray = []
+        OwnerID = ownerIDByUserName(UserName)
+        OwnersSiteIDs = sitesByOwnerID(OwnerID)
+        LoginDict = {"OwnerID" : ownerIDByUserName(UserName)}
+        for MPM in OwnersSiteIDs:
+            #MPMArrayofDicts = []
+            #MPMArrayofDicts = (CordinatorStatusbySiteID(MPM)) + (SiteInfobySiteID(MPM))
+            #MPMArrayofDicts.append({"Swipers": swipersBySiteID(MPM)})
+            #MPMArrayofDicts.append(CordinatorStatusbySiteID(MPM))
+            #MPMArrayofDicts.append(SiteInfobySiteID(MPM))
+            SitesArray.append({**(CordinatorStatusbySiteID(MPM)),
+            **(SiteInfobySiteID(MPM)), **{"Swipers": swipersBySiteID(MPM)},
+            **{"siteID" : MPM}})
+        LoginDict["SiteArray"] = SitesArray
+        return jsonify(LoginDict)
 
 
 @app.route('/swipersstatus=<SiteID>', methods=['GET'])
 def swipersstatus(SiteID):
-    Swipers = {"Swipers" : swipersBySiteID(SiteID)}
-    return jsonify(Swipers)
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        Swipers = {"Swipers" : swipersBySiteID(SiteID)}
+        return jsonify(Swipers)
 
 
 @app.route('/swipersconfig/site=<SiteID>/swiper=<MacAddress>', methods=['GET'])
 def swipersconfig(SiteID,MacAddress):
-    return jsonify(swiperconfigBySiteIDMAC(SiteID, MacAddress))
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        return jsonify(swiperconfigBySiteIDMAC(SiteID, MacAddress))
 
 
 @app.route('/purchases=<SiteID>', methods=['GET'])
 def purchases(SiteID):
-    purchases = {"purchases" : purchasebySiteID(SiteID)}
-    return jsonify(purchases)
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        purchases = {"purchases" : purchasebySiteID(SiteID)}
+        return jsonify(purchases)
 
 
 @app.route('/coordinatorstatus=<SiteID>', methods=['GET'])
 def coordinatorstatus(SiteID):
-    return jsonify(CordinatorStatusbySiteID(SiteID))
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        return jsonify(CordinatorStatusbySiteID(SiteID))
 
 
 @app.route('/alarmcontacts=<SiteID>', methods=['GET'])
 def alarmcontacts(SiteID):
-    return jsonify(AlarmContactsbySiteID(SiteID))
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        return jsonify(AlarmContactsbySiteID(SiteID))
 
 
 @app.route('/transaction=<purchID>', methods=['GET'])
 def transaction(purchID):
-    transaction = {"transactions" : transactionsbypurchaseid(purchID)}
-    return jsonify(transaction)
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        transaction = {"transactions" : transactionsbypurchaseid(purchID)}
+        return jsonify(transaction)
 
 
 @app.route('/siteinfo=<siteID>', methods=['GET'])
 def siteinfo(siteID):
-    return jsonify(SiteInfobySiteID(siteID))
+    isKeyCorrect = keyCheck()
+    if isKeyCorrect != True:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    else:
+        return jsonify(SiteInfobySiteID(siteID))
 
 
 if __name__ == '__main__':
